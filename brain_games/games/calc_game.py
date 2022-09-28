6bbe57debd0a8bc1eea6c47a78915784fc96b5e5
88ArtemSmirnov88 <artembanchik@mail.ru>
2022-09-28 11:48:26 +0300

    'fixed errors'

--- brain_games/games/calc_game.py
+++ brain_games/games/calc_game.py
@@ -8,5 +8,10 @@ def calc():
     num_two = randint(1, 10)
     question = (f'''What is the result of the expression?
 Question: {num_one} {operator} {num_two}''')
-    correct_answer = str(f'{num_one}{operator}{num_two}')
+    if operator == '*':
+        correct_answer = f'{num_one} * {num_two}'
+    elif operator == '+':
+        correct_answer = f'{num_one} + {num_two}'
+    else:
+        correct_answer = f'{num_one} - {num_two}'
     return question, correct_answer
