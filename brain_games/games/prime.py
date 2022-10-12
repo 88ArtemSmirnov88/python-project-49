e72abae25dceb14f3e081ac558aade891a33914e
88ArtemSmirnov88 <artembanchik@mail.ru>
2022-10-12 14:23:32 +0300

    'fixed errors'

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -1,7 +1,8 @@
 from random import randint
 
 
-DESCRIPTION = '''Answer "yes" if given number is prime. Otherwise answer "no".'''
+DESCRIPTION = '''Answer "yes" if given number is prime.
+                 Otherwise answer "no".'''
 
 
 def is_prime(number):
